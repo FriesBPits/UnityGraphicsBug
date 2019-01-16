ff3bebf298684bca350a2762a5fe2ffd28e9747a
Fries Boury <fries@playitsafe.eu>
2019-01-16 12:58:54 +0100

    Added Project source

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, _Speed * Time.deltaTime, 0f);
+    }
+}
---
+++ Assets/SetActiveOnInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveOnInput : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _target.SetActive(!_target.activeSelf);
+        }
+    }
+}
